7d903e6c6dd54f20749e766a64bd3a8f3eaf156d
eerotal <eerotal@users.noreply.github.com>
2017-10-21 11:09:15 +0300

    Remove header parser, add more symbols + separate them into own file
    - Remove the header parsing since that really isn't needed as it isn't
      a part of the original language syntax.
    - Add more symbols and separate all the symbol tables into the
      file symbols.py that's imported into the main nsprep.py file.
    - Make nsprep a python package by creating the __init__.py file.

---
+++ nsprep/symbols.py
@@ -0,0 +1,60 @@
+#!/bin/python
+
+symbols_assignment = {
+	'->':		u'\u2192',
+	'~=':		u'\u2248'
+}
+
+symbols_logic = {
+	'!=':		u'\u2260',
+	'==':		u'\u003D',
+	'<=':		u'\u2264',
+	'>=':		u'\u2265'
+}
+
+sym_misc_prefix = '_s_';
+symbols_misc = {
+	'inf': 		u'\u221e',
+	'plusminus': 	u'\u00b1',
+	'minusplus': 	u'\u2213',
+	'cross': 	u'\u00d7',
+	'sqrt': 	u'\u221a',
+	'cubebrt':	u'\u221b',
+	'fourthrt':	u'\u221c',
+	'integral':	u'\u222b',
+	'therefore':	u'\u2234',
+	'because':	u'\u2235',
+	'hdots':	u'\u2026',
+	'vdots':	u'\u22ee',
+	'drdots':	u'\u22f1',
+	'urdots':	u'\u22f0',
+	'box': 		u'\u220e'
+}
+
+sym_greek_prefix = '_g_';
+symbols_greek = {         # LOWER      UPPER
+	'lpha': 	[u'\u03b1', u'\u0391'],
+	'beta': 	[u'\u03b2', u'\u0392'],
+	'gamma': 	[u'\u03b3', u'\u0393'],
+	'delta': 	[u'\u03b4', u'\u0394'],
+	'epsilon': 	[u'\u03b5', u'\u0395'],
+	'zeta': 	[u'\u03b6', u'\u0396'],
+	'eta': 		[u'\u03b7', u'\u0397'],
+	'theta': 	[u'\u03b8', u'\u0398'],
+	'iota': 	[u'\u03b9', u'\u0399'],
+	'kappa': 	[u'\u03ba', u'\u039a'],
+	'lambda': 	[u'\u03bb', u'\u039b'],
+	'mu': 		[u'\u03bc', u'\u039c'],
+	'nu': 		[u'\u03bd', u'\u039d'],
+	'xi': 		[u'\u03be', u'\u039e'],
+	'omicron': 	[u'\u03bf', u'\u039f'],
+	'pi': 		[u'\u03c0', u'\u03a0'],
+	'rho': 		[u'\u03c1', u'\u03a1'],
+	'sigma': 	[u'\u03c3', u'\u03a3'],
+	'tau': 		[u'\u03c4', u'\u03a4'],
+	'upsilon': 	[u'\u03c5', u'\u03a5'],
+	'phi': 		[u'\u03c6', u'\u03a6'],
+	'chi': 		[u'\u03c7', u'\u03a7'],
+	'psi': 		[u'\u03c8', u'\u03a8'],
+	'omega': 	[u'\u03c9', u'\u03a9']
+}
